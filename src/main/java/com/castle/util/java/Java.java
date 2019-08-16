76c9460043c4a0af1a4ccef88d60ff5087b567eb
Tom Tzook <tomtzook@gmail.com>
2019-08-16 15:45:52 +0300

    current jar finder

---
+++ src/main/java/com/castle/util/java/Java.java
@@ -0,0 +1,35 @@
+package com.castle.util.java;
+
+import com.castle.nio.zip.Zip;
+
+import java.io.File;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+
+public class Java {
+
+    private Java() {}
+
+    /**
+     * <p>
+     *     Get the <em>JAR</em> archive containing the given class
+     *     code file.
+     * </p>
+     *
+     * @param cls class type
+     *
+     * @return the <em>JAR</em> as a {@link Zip} object.
+     * @throws Error if unable to find the <em>JAR</em>. Might occur if the
+     *      class is not contained in a <em>JAR</em>.
+     */
+    public static Zip currentJar(Class<?> cls) {
+        try {
+            Path path = new File(cls.getProtectionDomain().getCodeSource().getLocation()
+                    .toURI()).toPath();
+
+            return new Zip(path);
+        } catch (URISyntaxException e) {
+            throw new Error(e);
+        }
+    }
+}
