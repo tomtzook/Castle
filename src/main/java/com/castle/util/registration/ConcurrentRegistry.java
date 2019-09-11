f586ccec830f750949273ab2a57d30e58ead6176
Tom Tzook <tomtzook@gmail.com>
2019-09-11 21:18:13 +0300

    added registered

---
+++ src/main/java/com/castle/util/registration/ConcurrentRegistry.java
@@ -0,0 +1,31 @@
+package com.castle.util.registration;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class ConcurrentRegistry<T> implements Registry<T> {
+
+    private final Collection<T> mRegistered;
+
+    public ConcurrentRegistry() {
+        mRegistered = new ArrayList<>();
+    }
+
+    @Override
+    public boolean register(T element) {
+        synchronized (mRegistered) {
+            return mRegistered.add(element);
+        }
+    }
+
+    @Override
+    public Collection<T> getRegistered(boolean clearRegistry) {
+        Collection<T> copy;
+        synchronized (mRegistered) {
+            copy = new ArrayList<>(mRegistered);
+            mRegistered.clear();
+        }
+
+        return copy;
+    }
+}
