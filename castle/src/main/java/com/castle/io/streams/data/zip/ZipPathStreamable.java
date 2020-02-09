664b92b6a8f545b7ae6cca3ee1828094971a37c5
Tom Tzook <tomtzook@gmail.com>
2020-02-09 02:19:20 +0200

    streamable improvements

--- castle/src/main/java/com/castle/io/streams/data/zip/ZipPathStreamable.java
+++ castle/src/main/java/com/castle/io/streams/data/zip/ZipPathStreamable.java
@@ -6,27 +6,33 @@ import com.castle.io.streams.data.ReadOnlyStreamable;
 import com.castle.nio.temp.TempPath;
 import com.castle.nio.zip.OpenZip;
 import com.castle.nio.zip.Zip;
+import com.castle.util.function.ThrowingFunction;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Path;
+import java.util.function.Function;
 import java.util.regex.Pattern;
 
 @ThreadSafe
 public class ZipPathStreamable implements ReadOnlyStreamable {
 
     private final Zip mZip;
-    private final Pattern mPattern;
+    private final ThrowingFunction<OpenZip, Path, IOException> mPathFinder;
+
+    public ZipPathStreamable(Zip zip, ThrowingFunction<OpenZip, Path, IOException> pathFinder) {
+        mZip = zip;
+        mPathFinder = pathFinder;
+    }
 
     public ZipPathStreamable(Zip zip, Pattern pattern) {
-        mZip = zip;
-        mPattern = pattern;
+        this(zip, (openZip) -> openZip.findFile(pattern));
     }
 
     @Override
     public InputStream openRead() throws IOException {
         try(OpenZip openZip = mZip.open()) {
-            Path path = openZip.findFile(mPattern);
+            Path path = mPathFinder.apply(openZip);
             TempPath tempPath = openZip.extract(path);
 
             return new TempPathInputStream(tempPath);
--- castle/src/main/java/com/castle/util/function/ThrowingRunnable.java
+++ castle/src/main/java/com/castle/util/function/ThrowingRunnable.java
@@ -1,7 +1,13 @@
 package com.castle.util.function;
 
+import com.castle.util.function.exceptions.FunctionException;
+
 @FunctionalInterface
 public interface ThrowingRunnable<R, E extends Exception> {
+
+    @FunctionalInterface
+    interface Generic<R> extends ThrowingRunnable<R, FunctionException> {
+    }
 
     R run() throws E;
 }
