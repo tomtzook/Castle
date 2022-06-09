9771d79cf9d356fd893e3284deb14e7713af52e1
Tom Tzook <tomtzook@gmail.com>
2022-06-09 07:32:46 +0300

    improved serializer

--- castle.io/src/main/java/com/castle/io/SerializedType.java
+++ castle.io/src/main/java/com/castle/io/SerializedType.java
@@ -1,22 +1,35 @@
 package com.castle.io;
 
 public enum SerializedType {
-    NULL(0),
-    INTEGER(1),
-    DOUBLE(2),
-    STRING(3),
-    BOOLEAN(4),
-    BLOB(5)
+    NULL(0, null),
+    BYTE(1, Byte.class),
+    SHORT(2, Short.class),
+    INTEGER(3, Integer.class),
+    LONG(4, Long.class),
+    FLOAT(5, Float.class),
+    DOUBLE(6, Double.class),
+    CHAR(7, Character.class),
+    STRING(8, String.class),
+    BOOLEAN(9, Boolean.class),
+    BLOB(10, byte[].class),
+    ARRAY(11, null),
+    OBJECT(12, Object.class)
     ;
 
     private final int mIntValue;
+    private final Class<?> mJavaType;
 
-    SerializedType(int intValue) {
+    SerializedType(int intValue, Class<?> javaType) {
         mIntValue = intValue;
+        mJavaType = javaType;
     }
 
     public int intValue() {
         return mIntValue;
+    }
+
+    public Class<?> javaType() {
+        return mJavaType;
     }
 
     public static SerializedType fromInt(int typeInt) {
@@ -27,5 +40,15 @@ public enum SerializedType {
         }
 
         throw new EnumConstantNotPresentException(SerializedType.class, String.valueOf(typeInt));
+    }
+
+    public static SerializedType fromJavaType(Class<?> javaType) {
+        for (SerializedType type : values()) {
+            if (type.javaType() != null && type.javaType().isAssignableFrom(javaType)) {
+                return type;
+            }
+        }
+
+        throw new EnumConstantNotPresentException(SerializedType.class, javaType.getName());
     }
 }
--- castle.io/src/main/java/com/castle/io/TypedSerializer.java
+++ castle.io/src/main/java/com/castle/io/TypedSerializer.java
@@ -3,7 +3,9 @@ package com.castle.io;
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class TypedSerializer {
@@ -20,12 +22,27 @@ public class TypedSerializer {
     public void writeTyped(DataOutput output, Object value) throws IOException {
         if (value == null) {
             output.writeInt(SerializedType.NULL.intValue());
+        } else if (value instanceof Byte) {
+            output.writeInt(SerializedType.BYTE.intValue());
+            output.writeByte((Byte) value);
+        } else if (value instanceof Short) {
+            output.writeInt(SerializedType.SHORT.intValue());
+            output.writeShort((Short) value);
         } else if (value instanceof Integer) {
             output.writeInt(SerializedType.INTEGER.intValue());
             output.writeInt((Integer) value);
+        } else if (value instanceof Long) {
+            output.writeInt(SerializedType.LONG.intValue());
+            output.writeLong((Long) value);
+        } else if (value instanceof Float) {
+            output.writeInt(SerializedType.FLOAT.intValue());
+            output.writeFloat((Float) value);
         } else if (value instanceof Double) {
             output.writeInt(SerializedType.DOUBLE.intValue());
             output.writeDouble((Double) value);
+        } else if (value instanceof Character) {
+            output.writeInt(SerializedType.CHAR.intValue());
+            output.writeChar((Character) value);
         } else if (value instanceof String) {
             output.writeInt(SerializedType.STRING.intValue());
             output.writeUTF((String) value);
@@ -36,6 +53,19 @@ public class TypedSerializer {
             output.writeInt(SerializedType.BLOB.intValue());
             output.writeInt(((byte[])value).length);
             output.write((byte[]) value);
+        } else if (value.getClass().isArray()) {
+            output.writeInt(SerializedType.ARRAY.intValue());
+            int length = Array.getLength(value);
+            output.writeInt(length);
+            SerializedType innerType = SerializedType.fromJavaType(value.getClass().getComponentType());
+            output.writeInt(innerType.intValue());
+
+            for (int i = 0; i < length; i++) {
+                writeTyped(output, Array.get(value, i));
+            }
+        } else if (value instanceof List) {
+            //noinspection rawtypes
+            writeTyped(output, ((List)value).toArray());
         } else {
             throw new IOException("unsupported type: " + value.getClass().getName());
         }
@@ -60,8 +90,13 @@ public class TypedSerializer {
 
         switch (type) {
             case NULL: return null;
+            case BYTE: return input.readByte();
+            case SHORT: return input.readShort();
             case INTEGER: return input.readInt();
+            case LONG: return input.readLong();
+            case FLOAT: return input.readFloat();
             case DOUBLE: return input.readDouble();
+            case CHAR: return input.readChar();
             case STRING: return input.readUTF();
             case BOOLEAN: return input.readBoolean();
             case BLOB: {
@@ -69,6 +104,19 @@ public class TypedSerializer {
                 byte[] data = new byte[length];
                 input.readFully(data);
                 return data;
+            }
+            case ARRAY: {
+                int length = input.readInt();
+                int innerTypeInt = input.readInt();
+                SerializedType innerType = SerializedType.fromInt(innerTypeInt);
+
+                Object arr = Array.newInstance(innerType.javaType(), length);
+                for (int i = 0; i < length; i++) {
+                    Object value = readTyped(input);
+                    Array.set(arr, i, value);
+                }
+
+                return arr;
             }
             default: throw new IOException("unsupported type: " + type.name());
         }
