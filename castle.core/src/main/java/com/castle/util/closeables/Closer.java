4dfbccf2053f8527d32e09a037223519da559ba3
Tom Tzook <tomtzook@gmail.com>
2022-06-12 20:23:33 +0300

    docs for closer

--- castle.core/src/main/java/com/castle/util/closeables/Closer.java
+++ castle.core/src/main/java/com/castle/util/closeables/Closer.java
@@ -10,6 +10,36 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Deque;
 
+/**
+ * A utility for managing multiple {@link AutoCloseable} (or {@link java.io.Closeable}
+ * objects.
+ *
+ * Allows registration of <em>resources</em> which will all be closed upon invocations
+ * of {@link #close()} or as a result of using {@link #run(ThrowingSupplier, Class, CloseOption)}.
+ *
+ * <p>
+ *     Registering <em>resources</em> on the fly.
+ *     <pre>
+ *        try (Closer closer = Closer.empty()) {
+ *              // do some stuff
+ *              closer.add(resource1);
+ *              // do some other stuff
+ *              closer.add(resource2);
+ *              // do more stuff
+ *         }
+ *     </pre>
+ *     <em>resource1</em> and <em>resource2</em> will both be closed.
+ * </p>
+ * <p>
+ *     Alternatively, using {@link #run(ThrowingSupplier, Class, CloseOption) run}
+ *     <pre>
+*          Closer closer = Closer.with(resource);
+ *         closer.run(()-> {
+ *             // do some stuff with resource
+ *         }, ExpectedException.class, Closer.CloserOption.ON_ERROR);
+ *     </pre>
+ * </p>
+ */
 @NotThreadSafe
 public class Closer implements AutoCloseable {
 
@@ -53,7 +83,11 @@ public class Closer implements AutoCloseable {
     }
 
     public static Closer with(AutoCloseable... closeables) {
-        return new Closer(new ArrayDeque<>(Arrays.asList(closeables)));
+        return with(Arrays.asList(closeables));
+    }
+
+    public static Closer with(Collection<? extends AutoCloseable> closeables) {
+        return new Closer(new ArrayDeque<>(closeables));
     }
 
     public Closer add(AutoCloseable closeable) {
@@ -65,11 +99,30 @@ public class Closer implements AutoCloseable {
         return addAll(Arrays.asList(closeables));
     }
 
-    public <T extends AutoCloseable> Closer addAll(Collection<T> closeables) {
-        closeables.forEach(this::add);
+    public Closer addAll(Collection<? extends AutoCloseable> closeables) {
+        mCloseables.addAll(closeables);
         return this;
     }
 
+    /**
+     * Executes a given function utilizing registered resources.
+     *
+     * @param consumer function to execute
+     * @param exceptionType error expected to be thrown by the function.
+     * @param closeOption when to close the registered resources.
+     *                    <ul>
+     *                      <li>if {@link CloseOption#ALWAYS}, {@link #close()}
+     *                      will always be invoked after calling the given function.</li>
+     *                      <li>if {@link CloseOption#ON_ERROR}, {@link #close()}
+     *                      will be invoked if the given function throws an exception.</li>
+     *                      <li>{@link CloseOption#ON_SUCCESS}, {@link #close()}
+     *                      will be invoked if the given function does not throw an exception.</li>
+     *                    </ul>
+     * @param <R> expected return type.
+     * @param <E> expected exception type.
+     * @return result from the given function.
+     * @throws E from the function, if it throws one.
+     */
     public <R, E extends Exception> R run(ThrowingSupplier<R, E> consumer, Class<E> exceptionType, CloseOption closeOption) throws E {
         checkIsClosed();
 
@@ -99,6 +152,15 @@ public class Closer implements AutoCloseable {
         throw new IllegalStateException();
     }
 
+    /**
+     * Closes all the registered resources, by invoking {@link AutoCloseable#close() close} on each.
+     * If one of the resources produces an exception during <em>close</em>, the exception is stored
+     * aside until all the other resources has being close, and only then is it thrown.
+     *
+     * Resources are closed in reverse order to their registration.
+     *
+     * @throws Exception if one of the resources throws an exception while {@link AutoCloseable#close() closing} it.
+     */
     @Override
     public void close() throws Exception {
         checkIsClosed();
